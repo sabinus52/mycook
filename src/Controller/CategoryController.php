70150e35f54f6d567a81f3d29e7315f0014d66bb
Sabinus52 <sabinus52@gmail.com>
2021-04-10 16:10:20 +0200

    CRUD des catégories

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Controleur de gestion des catégories
+ *
+ * @author Olivier <sabinus52@gmail.com>
+ *
+ * @package MyCook
+ */
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/category")
+ */
+class CategoryController extends AbstractController
+{
+
+    /**
+     * Liste des catégories
+     * 
+     * @Route("/", name="category_index", methods={"GET"})
+     */
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+
+    /**
+     * Ajout d'une nouvelle catégorie
+     * 
+     * @Route("/create", name="category_create", methods={"GET","POST"})
+     */
+    public function create(Request $request): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->render('category/new.html.twig', [
+            'category' => $category,
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * Visualiser la catégorie
+     * 
+     * @Route("/{id}", name="category_show", methods={"GET"})
+     */
+    public function show(Category $category): Response
+    {
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+        ]);
+    }
+
+
+    /**
+     * Editer la catégorie
+     * 
+     * @Route("/{id}/edit", name="category_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Category $category): Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->render('category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * Supprime une catégorie
+     * 
+     * @Route("/{id}", name="category_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Category $category): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($category);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('category_index');
+    }
+}
---
+++ src/Form/CategoryType.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Formulaire des catégories de recettes
+ *
+ * @author Olivier <sabinus52@gmail.com>
+ *
+ * @package MyCook
+ */
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
