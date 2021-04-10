dc044c4745e84605b907244d41c241b3c8c7572d
Sabinus52 <sabinus52@gmail.com>
2021-04-10 16:35:26 +0200

    CRUD des ingrédients

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Controleur de la gestion des ingrédients
+ *
+ * @author Olivier <sabinus52@gmail.com>
+ *
+ * @package MyCook
+ */
+
+namespace App\Controller;
+
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
+use App\Repository\IngredientRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/ingredient")
+ */
+class IngredientController extends AbstractController
+{
+
+    /**
+     * Index ou liste des ingrédients
+     * 
+     * @Route("/", name="ingredient_index", methods={"GET"})
+     */
+    public function index(IngredientRepository $ingredientRepository): Response
+    {
+        return $this->render('ingredient/index.html.twig', [
+            'ingredients' => $ingredientRepository->findAll(),
+        ]);
+    }
+
+
+    /**
+     * Création d'un nouvel ingrédient
+     * 
+     * @Route("/create", name="ingredient_create", methods={"GET","POST"})
+     */
+    public function create(Request $request): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($ingredient);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('ingredient_index');
+        }
+
+        return $this->render('ingredient/new.html.twig', [
+            'ingredient' => $ingredient,
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * Visualisation d'un ingrédient
+     * 
+     * @Route("/{id}", name="ingredient_show", methods={"GET"})
+     */
+    public function show(Ingredient $ingredient): Response
+    {
+        return $this->render('ingredient/show.html.twig', [
+            'ingredient' => $ingredient,
+        ]);
+    }
+
+
+    /**
+     * Edition d'un ingrédient
+     * 
+     * @Route("/{id}/edit", name="ingredient_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Ingredient $ingredient): Response
+    {
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('ingredient_index');
+        }
+
+        return $this->render('ingredient/edit.html.twig', [
+            'ingredient' => $ingredient,
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * Suppression d'un ingrédient
+     * @Route("/{id}", name="ingredient_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Ingredient $ingredient): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$ingredient->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($ingredient);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('ingredient_index');
+    }
+
+}
---
+++ src/Form/IngredientType.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Formulaire des ingrédients
+ *
+ * @author Olivier <sabinus52@gmail.com>
+ *
+ * @package MyCook
+ */
+
+namespace App\Form;
+
+use App\Entity\Ingredient;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class IngredientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ingredient::class,
+        ]);
+    }
+}
