ef823d83af616fef6d76b3552c552551e8d55b66
Sabinus52 <sabinus52@gmail.com>
2021-04-12 14:26:44 +0200

    Config Form Bootstrap

--- src/Form/IngredientType.php
+++ src/Form/IngredientType.php
@@ -13,13 +13,16 @@ use App\Entity\Ingredient;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class IngredientType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
         ;
     }
 
