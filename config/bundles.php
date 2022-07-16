8337afc95af4ba292726ec92294d3883b3bedc87
Sabinus52 <sabinus52@gmail.com>
2022-07-16 18:15:47 +0200

    Ajout du bundle OlixBackOffice

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
+    Olix\BackOfficeBundle\OlixBackOfficeBundle::class => ['all' => true],
 ];
